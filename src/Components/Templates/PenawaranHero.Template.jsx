36872e8f423da97c7374162c9f37940b3c856497
novalchristian <novalchristian41@gmail.com>
2022-06-24 16:52:27 +0700

    membuat penawaran main

--- src/Components/Templates/PenawaranHero.Template.jsx
+++ src/Components/Templates/PenawaranHero.Template.jsx
@@ -5,9 +5,9 @@ import { TitleHeroAtom, ButtonAtom} from '../Atoms';
 
 export default function PenawaranHeroTemplate() {
   return (
-    <div className="md:pt-22 pt-28 md:pb-16 flex justify-center">
+    <div className="md:pt-22 pt-28 md:pb-12 flex justify-center ">
       <div className="container flex justify-center">
-        <div className="w-full max-w-7xl">
+        <div className="w-full max-w-[1340px]">
           <div className="flex flex-wrap-reverse justify-center">
             <div className="w-full lg:w-1/2 md:pt-10 flex md:justify-start justify-center mt-[-30px] md:mt-0">
               <img
@@ -18,7 +18,7 @@ export default function PenawaranHeroTemplate() {
             </div>
             <div className="w-full lg:w-1/2 flex flex-col text-end items-end text-right justify-center px-10 md:px-0 pt-5 md:pt-0">
               <TitleHeroAtom>Penawaran</TitleHeroAtom>
-              <p className="max-w-md text-base text-secondary pt-3">
+              <p className="max-w-[430px] text-base text-secondary pt-3">
                 PT Pegiska Berkat Abadi memiliki beberapa penawaran berdasarkan
                 jenis jasa yang diinginkan. PT Pegiska Berkat Abadi dapat dengan
                 baik mengurus semuanya berdasarkan jasa yang diminta.
--- src/Pages/Penawaran.jsx
+++ src/Pages/Penawaran.jsx
@@ -12,6 +12,90 @@ function Penawaran() {
       <PenawaranHeroTemplate />
       {/* Penawaran Hero End */}
 
+      {/* Main Penawaran Start */}
+      <div className="md:pt-0 pt-16 pb-10 flex justify-center">
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="mx-auto mb-16 text-center max-w-6xl ">
+              <div className="pb-10">
+                <h4 className="mb-2 text-lg font-bold text-primary uppercase">
+                  Offer
+                </h4>
+                <h2 className="mb-4 text-3xl font-bold text-black md:text-[2.5rem]">
+                  Penawaran Jasa Perusahaan
+                </h2>
+                <p className="mt-5 font-light text-secondary text-sm md:text-xl underline underline-offset-8 decoration-1">
+                  <span className="text-red-600">Keterangan :</span> Login jika
+                  ingin melihat harga dari setiap jasa !!!
+                </p>
+              </div>
+
+              <div className="flex justify-between md:pt-10 md:md:pb-6">
+                <h4 className="text-primary font-bold">Keterangan Jasa</h4>
+                <h4 className="font-bold text-black pl-6">20 Feet</h4>
+                <h4 className="font-bold text-black pr-16">40 Feet</h4>
+              </div>
+
+              <div className="border-b-2 pb-4">
+                <div className="flex justify-between items-center md:pt-4">
+                  <h4 className="text-black font-bold">
+                    Jasa Inklaring{" "}
+                    <span className="font-light">(Container 1 -3)</span>
+                  </h4>
+                  <p className="font-bold text-[#C1C1C1] text-left max-w-[190px]">
+                    Rp XXX.XXX/Container{" "}
+                    <span className="font-light">(s/d container ke-3)</span>
+                  </p>
+                  <p className="font-bold text-[#C1C1C1] max-w-[190px] w-[240px] text-left">
+                    Rp XXX.XXX/Container{" "}
+                    <span className="font-light">(s/d container ke-3)</span>
+                  </p>
+                </div>
+                <div className="flex justify-between items-center md:pt-4">
+                  <h4 className="text-black font-bold w-[240px] text-left">
+                    Container berikutnya
+                  </h4>
+                  <p className="font-bold text-[#C1C1C1] text-left max-w-[190px]">
+                    Rp XXX.XXX/Container
+                  </p>
+                  <p className="font-bold text-[#C1C1C1] text-left max-w-[190px]">
+                    Rp XXX.XXX/Container
+                  </p>
+                </div>
+              </div>
+
+              <div className="border-b-2 pb-4">
+                <div className="flex justify-between items-center md:pt-4">
+                  <h4 className="text-black font-bold">
+                    Jasa Inklaring{" "}
+                    <span className="font-light">(Container 1 -3)</span>
+                  </h4>
+                  <p className="font-bold text-[#C1C1C1] text-left max-w-[190px]">
+                    Rp XXX.XXX/Container{" "}
+                    <span className="font-light">(s/d container ke-3)</span>
+                  </p>
+                  <p className="font-bold text-[#C1C1C1] max-w-[190px] w-[240px] text-left">
+                    Rp XXX.XXX/Container{" "}
+                    <span className="font-light">(s/d container ke-3)</span>
+                  </p>
+                </div>
+                <div className="flex justify-between items-center md:pt-4">
+                  <h4 className="text-black font-bold w-[240px] text-left">
+                    Container berikutnya
+                  </h4>
+                  <p className="font-bold text-[#C1C1C1] text-left max-w-[190px]">
+                    Rp XXX.XXX/Container
+                  </p>
+                  <p className="font-bold text-[#C1C1C1] text-left max-w-[190px]">
+                    Rp XXX.XXX/Container
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Main Penawaran End */}
 
       {/* Footer Template Start */}
       <FooterTemplate />
